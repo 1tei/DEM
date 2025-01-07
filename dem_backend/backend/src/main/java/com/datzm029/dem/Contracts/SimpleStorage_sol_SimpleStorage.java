223944f6dfb0921b6beec117652c6ea51fa28440
Aivars Orups <aivars.orups@lu.lv>
2025-01-07 19:17:04 +0200

    added trading fnc

--- dem_backend/backend/src/main/java/com/datzm029/dem/Contracts/SimpleStorage_sol_SimpleStorage.java
+++ dem_backend/backend/src/main/java/com/datzm029/dem/Contracts/SimpleStorage_sol_SimpleStorage.java
@@ -129,9 +129,6 @@ public class SimpleStorage_sol_SimpleStorage extends Contract {
         return deployRemoteCall(SimpleStorage_sol_SimpleStorage.class, web3j, transactionManager, gasPrice, gasLimit, getDeploymentBinary(), "");
     }
 
-//    public static void linkLibraries(List<Contract.LinkReference> references) {
-//        librariesLinkedBinary = linkBinaryWithReferences(BINARY, references);
-//    }
 
     private static String getDeploymentBinary() {
         if (librariesLinkedBinary != null) {
